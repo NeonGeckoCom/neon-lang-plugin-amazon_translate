82671b5e1097b71168fba2344aac45c65d35fcc3
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2021-09-08 15:57:38 -0700

    Unit Tests, Automation, Updates (#2)
    * Add github automation
    Add unit tests
    Update setup.py and requirements for PyPI
    
    * Add missing test_requirements.txt
    
    * Fix typo in tests directory name
    
    * Fix lang_detect keys logic
    Update license in file headers
    
    * Update aws keys config to handle Neon standard formatting
    
    * Make region config reference safe
    
    * Update plugin entry points

--- setup.py
+++ setup.py
@@ -1,21 +1,77 @@
 #!/usr/bin/env python3
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Development System
+# All trademark and other rights reserved by their respective owners
+# Copyright 2008-2021 Neongecko.com Inc.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 from setuptools import setup
+from os import getenv, path
 
-PLUGIN_ENTRY_POINT = 'amazontranslate_plug = amazontranslate_neon_plugin:AmazonTranslatePlugin'
+PLUGIN_ENTRY_POINT = 'amazontranslate_plug = neon_lang_plugin_amazon_translate:AmazonTranslator'
+DETECT_PLUGIN_ENTRY_POINT = 'amazontranslate_detection_plug = neon_lang_plugin_amazon_translate:AmazonDetector'
 
-DETECT_PLUGIN_ENTRY_POINT = 'amazontranslate_detection_plug = amazontranslate_neon_plugin:AmazonTranslateDetectPlugin'
+
+def get_requirements(requirements_filename: str):
+    requirements_file = path.join(path.abspath(path.dirname(__file__)), "requirements", requirements_filename)
+    with open(requirements_file, 'r', encoding='utf-8') as r:
+        requirements = r.readlines()
+    requirements = [r.strip() for r in requirements if r.strip() and not r.strip().startswith("#")]
+
+    for i in range(0, len(requirements)):
+        r = requirements[i]
+        if "@" in r:
+            parts = [p.lower() if p.strip().startswith("git+http") else p for p in r.split('@')]
+            r = "@".join(parts)
+        if getenv("GITHUB_TOKEN"):
+            if "github.com" in r:
+                requirements[i] = r.replace("github.com", f"{getenv('GITHUB_TOKEN')}@github.com")
+    return requirements
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("./version.py", "r", encoding="utf-8") as v:
+    for line in v.readlines():
+        if line.startswith("__version__"):
+            if '"' in line:
+                version = line.split('"')[1]
+            else:
+                version = line.split("'")[1]
+
 
 setup(
-    name='amazontranslate_neon_plugin',
-    version='0.0.1',
-    description='',
-    url='',
-    author='JarbasAi',
-    author_email='jarbasai@mailfence.com',
-    license='Apache-2.0',
-    packages=['amazontranslate_neon_plugin'],
-    install_requires=["ovos-plugin-manager>=0.0.1a2",
-                      "boto3"],
+    name='neon-lang-plugin-amazon-translate',
+    version=version,
+    description='Amazon Translate Language Plugin',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/NeonGeckoCom/neon-lang-plugin-amazon_translate',
+    author='Neongecko',
+    author_email='developers@neon.ai',
+    license='BSD-3-Clause',
+    packages=['neon_lang_plugin_amazon_translate'],
+    install_requires=get_requirements("requirements.txt"),
     zip_safe=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
